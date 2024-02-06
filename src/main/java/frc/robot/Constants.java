d7ae31a178219b5b708bc940cd0c8d9d045c5fa0
MarcusKhoushi <155914511+MarcusKhoushi@users.noreply.github.com>
2024-02-05 20:35:54 -0500

    Base Intake Code (Not Done)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,14 +4,64 @@
 
 package frc.robot;
 
-/**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
- *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
- */
-public final class Constants {
-  //DO NOT USE THIS CLASS USE THE CONFIG STRUCTURE
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.signals.SensorDirectionValue;
+
+
+public final class Constants extends IntakeConfig{
+
+    public static final int INTAKE_ID = 20;
+
+
+    public static final InvertedValue INTAKE_INVERTED = InvertedValue.CounterClockwise_Positive;
+    public static final NeutralModeValue INTAKE_NEUTRAL_MODE = NeutralModeValue.Coast;
+
+
+    public static final boolean INTAKE_ENABLE_CURRENT_LIMIT = true;
+    public static final int INTAKE_SUPPLY_CURRENT_LIMIT = 20;
+    public static final int INTAKE_SUPPLY_CURRENT_THRESHOLD = 30;
+    public static final double INTAKE_SUPPLY_TIME_THRESHOLD = 0.1;
+
+
+    public static final double INTAKE_P = 0.01;
+    public static final double INTAKE_I = 0;
+    public static final double INTAKE_D = 0;
+    public static final double INTAKE_V = 0.4;
+
+
+    public static final double INTAKE_RESET = 0;
+    public static final double INTAKE_INTAKE = 70;
+    public static final double INTAKE_SHOOT = 0;
+    public static final double INTAKE_FEED_SHOOT = -70;
+    public static final double INTAKE_AMP = -10;
+
+    public static final int INTAKE_PIVOT_ID = 22;
+
+    public static final InvertedValue INTAKE_PIVOT_INVERTED = InvertedValue.CounterClockwise_Positive;
+    public static final NeutralModeValue INTAKE_PIVOT_NEUTRAL_MODE = NeutralModeValue.Brake;
+
+
+    public static final boolean INTAKE_PIVOT_ENABLE_CURRENT_LIMIT = true;
+    public static final int INTAKE_PIVOT_SUPPLY_CURRENT_LIMIT = 30;
+    public static final int INTAKE_PIVOT_SUPPLY_CURRENT_THRESHOLD = 30;
+    public static final double INTAKE_PIVOT_SUPPLY_TIME_THRESHOLD = 0.1;
+
+
+    public static final double INTAKE_PIVOT_P = 0.1;
+    public static final double INTAKE_PIVOT_I = 0;
+    public static final double INTAKE_PIVOT_D = 0;
+    public static final double INTAKE_PIVOT_V = 0.01;
+
+    public static final int INTAKE_PIVOT_RESET = 0;
+    public static final int INTAKE_PIVOT_INTAKE = 25;
+    public static final int INTAKE_PIVOT_CLIMB = 0;
+    public static final int INTAKE_PIVOT_FEED = 0;
+
+    public static final int INTAKE_CANCODER_ID = 2;
+
+    public static final SensorDirectionValue INTAKE_CANCODER_INVERT = SensorDirectionValue.Clockwise_Positive;
+
+    public static final double INTAKE_CANCODER_OFFSET = 0;
+
 }
